b199cd9e5e8130023e942eec5a9f4d7bcd8a61d6
Guillaume <guizanko@hotmail.fr>
2023-10-18 16:30:36 +0200

    ok

--- exercises/02-maths/01-calculator-one/script.js
+++ exercises/02-maths/01-calculator-one/script.js
@@ -14,17 +14,51 @@
 
     document.getElementById("addition").addEventListener("click", () => {
         // perform an addition
+        const num1 = parseFloat(document.getElementById("op-one").value);
+        const num2 = parseFloat(document.getElementById("op-two").value);
+
+  
+        const result = num1 + num2;
+
+    // Afficher le résultat
+    alert(`Résultat de l'addition : ${result}`);
     });
 
     document.getElementById("substraction").addEventListener("click", () => {
         // perform an substraction
+        const num1 = parseFloat(document.getElementById("op-one").value);
+        const num2 = parseFloat(document.getElementById("op-two").value);
+    
+        // Effectuer une soustraction
+        const result = num1 - num2;
+    
+        // Afficher le résultat
+        alert(`Résultat de la soustraction : ${result}`);
     });
 
     document.getElementById("multiplication").addEventListener("click", () => {
         // perform an multiplication
+        const num1 = parseFloat(document.getElementById("op-one").value);
+        const num2 = parseFloat(document.getElementById("op-two").value);
+
+    // Effectuer une multiplication
+    const result = num1 * num2;
+
+    // Afficher le résultat
+    alert(`Résultat de la multiplication : ${result}`);
     });
 
     document.getElementById("division").addEventListener("click", () => {
         // perform an division
+        const num1 = parseFloat(document.getElementById("op-one").value);
+    const num2 = parseFloat(document.getElementById("op-two").value);
+
+    // Effectuer une division
+    if (num2 === 0) {
+        alert("Division par zéro impossible !");
+    } else {
+        const result = num1 / num2;
+        alert(`Résultat de la division : ${result}`);
+    }
     });
 })();
